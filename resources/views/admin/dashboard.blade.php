839cafe241af9e738c7351ce18718cbf23f4daa1
Verdi <104150927+gioVerdiansyah@users.noreply.github.com>
2024-07-24 22:01:12 +0700

    fixs

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -59,15 +59,18 @@
                                             @method('DELETE')
                                             <button type="submit" class="btn btn-danger">Hapus</button>
                                         </form>
-                                        <a href="{{ route('admin.get-invitation', $item->id) }}" class="download-btn ms-3">
-                                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"
-                                                class="svgIcon">
-                                                <path
-                                                    d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z">
-                                                </path>
-                                            </svg>
-                                            <span class="icon2"></span>
-                                        </a>
+                                        @if ($item->kehadiran == 'hadir')
+                                            <a href="{{ route('admin.get-invitation', $item->id) }}"
+                                                class="download-btn ms-3">
+                                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"
+                                                    class="svgIcon">
+                                                    <path
+                                                        d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z">
+                                                    </path>
+                                                </svg>
+                                                <span class="icon2"></span>
+                                            </a>
+                                        @endif
                                     </td>
                                 </tr>
                             @empty
